16f05ae1b943b878b6febddd9ea4eacc636955ec
v4s1n <vasin.oleksii@gmail.com>
2024-09-09 22:52:24 +0200

    Testimonials

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -1,5 +1,14 @@
 import InfoBlock from "../common/InfoBlock/InfoBlock";
+import { testimonials } from "../../Data";
+import Polygon from "../../assets/icons/Polygon.svg";
 import "./Testimonials.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+export interface Testimonials {
+  text: string;
+  name: string;
+  job: string;
+}
 
 const Testimonials = () => {
   return (
@@ -9,6 +18,38 @@ const Testimonials = () => {
           title="Testimonials"
           description="Hear from Our Satisfied Clients: Read Our Testimonials to Learn More about Our Digital Marketing Services"
         />
+        <div className="content">
+          <Swiper
+            className=""
+            slidesPerView={2}
+            spaceBetween={50}
+            centeredSlides={true}
+            initialSlide={1}
+            breakpoints={{
+              900: { slidesPerView: 2 },
+              0: { slidesPerView: 1 },
+            }}
+          >
+            {testimonials.map(({ text, name, job }, i: number) => {
+              return (
+                <SwiperSlide>
+                  <div className="content__item" key={i}>
+                    <div className="top">
+                      <p>{text}</p>
+                    </div>
+                    <div className="bottom">
+                      <h5 className="name">{name}</h5>
+                      <p>{job}</p>
+                      <div className="polygon">
+                        <img src={Polygon} alt="polygon" />
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
       </div>
     </section>
   );
